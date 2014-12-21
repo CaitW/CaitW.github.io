04ebb5b70a5e188466642f3382477d96b5e2d7c6
CaitW <wolters.caitlin@gmail.com>
2014-12-21 10:01:57 -0600

    added Projects page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+$(document).ready(function () {
 
+	$(".internalLink").click(function (e) {
+
+		var pageToOpen = $(this).attr("name");
+		if(pageToOpen == "projects")
+		{
+			$(".page.open").removeClass("open");
+			$(".page[name=" + pageToOpen + "]").addClass("open");
+			$("#contents").css({
+				"top": "10%",
+				"bottom": "10%"
+			});
+		}
+		if($(this).attr("name") == "resume")
+		{
+
+		}
+
+	});
+
+	$(".title .fa").click(function() {
+		$(".page.open").removeClass("open");
+		$("#contents").removeAttr("style");
+		$(".page[name=home]").addClass("open");
+
+	});
+
+});
