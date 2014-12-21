1d826113a5e675fe41b2decc8a9267e3eb1bdaa2
CaitW <wolters.caitlin@gmail.com>
2014-12-21 10:28:55 -0600

    resume

--- js/main.js
+++ js/main.js
@@ -3,19 +3,13 @@ $(document).ready(function () {
 	$(".internalLink").click(function (e) {
 
 		var pageToOpen = $(this).attr("name");
-		if(pageToOpen == "projects")
-		{
-			$(".page.open").removeClass("open");
-			$(".page[name=" + pageToOpen + "]").addClass("open");
-			$("#contents").css({
-				"top": "10%",
-				"bottom": "10%"
-			});
-		}
-		if($(this).attr("name") == "resume")
-		{
+		$(".page.open").removeClass("open");
+		$(".page[name=" + pageToOpen + "]").addClass("open");
 
-		}
+		$("#contents").css({
+			"top": "10%",
+			"bottom": "10%"
+		});
 
 	});
 
